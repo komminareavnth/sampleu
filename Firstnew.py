de0ebaf989adb46b1261eaa065f9df84daba8223
komminareavnth <112478105+komminareavnth@users.noreply.github.com>
2022-09-05 10:26:42 +0530

    Update Firstnew.py

--- Firstnew.py
+++ Firstnew.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
 a=st.number_input('Enter any value')
 if st.button('click'):
-    st.success(f'Your lucky Number is ')
+    st.success(f'Your lucky Number is {a}')
     
